726b179494433c0002c33fedc72d0d55207a0bb2
admin <admin@reserves.ef.local>
2017-12-18 16:36:13 +0200

    Базовая разметка

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+<!--шапка    -->
+    <body>
+        <header>
+<!--шапка-->
+    Привет Шапка
+        <?php
+        // put your code here
+        ?>          
+        </header>
+<!--шапка-->
+<!--тело-->
+<div>
+    тело
+</div>
+<!--тело-->
+<!--Низ-->
+        <footer>
+    Привет футер        
+        </footer>
+<!--Низ-->
+    </body>
+</html>
