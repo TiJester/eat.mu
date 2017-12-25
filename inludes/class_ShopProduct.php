6989dde2b5624839ec485d5bc11f427c502c0fc6
Grib <Grib@192.168.0.26>
2017-12-25 18:51:30 +0200

    Наследование, расширение родительского класса

--- inludes/class_ShopProduct.php
+++ inludes/class_ShopProduct.php
@@ -12,5 +12,43 @@
  * @author Grib
  */
 class class_ShopProduct {
-    //put your code here
+    public $title;
+    public $price;
+    
+    public function __construct($title, $price) 
+    {
+        $this->title=$title;
+        $this->price=$price;
+    }
+    
+    public function getPrice()
+    {
+        return $this->price;
+    }
+    
+    public function getView()
+    {
+        $result = $this->title."<br />".$this->price;
+        return $result;
+    }
 }
+
+class class_DigitalProduct extends class_ShopProduct{ //дочерний класс
+    public $type;
+    public $size;
+    
+    public function __construct($title, $price, $type, $size) {
+        parent::__construct($title, $price);
+        $this->type=$type;
+        $this->size=$size;        
+    }
+    
+    public function getView() {
+        $result = parent::getView();
+        $result .= "<br />".$this->type."<br />".$this->size;
+        return $result;
+    }
+}
+
+$cd = new class_DigitalProduct("Metallica", 249, "CD", 680);
+echo $cd->getView();
