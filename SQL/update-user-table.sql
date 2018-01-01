c4722ce2f32e535391228dd12710a741ec4d6c41
Grib <Grib@192.168.0.26>
2018-01-01 22:21:26 +0200

    Без обновления!

--- SQL/update-user-table.sql
+++ SQL/update-user-table.sql
@@ -4,14 +4,5 @@
  */
 /**
  * Author:  Grib
- * Created: 01.01.2018
+ * Created: 01.01.2018:22.20
  */
-
-ALTER TABLE `user` 
-    ADD `address_country` CHAR(70) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'адрес (страна)' AFTER `password`, ADD UNIQUE (`address_country`),
-    ADD `address_city` CHAR(70) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'адрес (город)' AFTER `address_country`, ADD UNIQUE (`address_city`),
-    ADD `address_street` CHAR(70) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'адрес (улица)' AFTER `address_city`, ADD UNIQUE (`address_street`),
-    ADD `address_street_num` CHAR(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'адрес (номер по улице)' AFTER `address_street`, ADD UNIQUE (`address_street_num`),
-    ADD `address_apartment` CHAR(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'адрес (номер квартиры)' AFTER `address_street_num`, ADD UNIQUE (`address_apartment`);
-
-
