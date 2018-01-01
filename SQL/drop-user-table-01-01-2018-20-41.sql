1a72d7b4d98a4946325cbe7931242a189e1580ef
Grib <Grib@192.168.0.26>
2018-01-01 20:43:10 +0200

    Удаление из табилцы user информации об адресе drop

---
+++ SQL/drop-user-table-01-01-2018-20-41.sql
@@ -0,0 +1,15 @@
+/* 
+ * TiJester
+ * UA Odessa  * 
+ */
+/**
+ * Author:  Grib
+ * Created: 01.01.2018
+ */
+
+ALTER TABLE `user`
+  DROP `address_country`,
+  DROP `address_city`,
+  DROP `address_street`,
+  DROP `address_street_num`,
+  DROP `address_apartment`;
