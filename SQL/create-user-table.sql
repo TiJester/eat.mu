43d8fe2fdb4000d9bb0212875a9434f6713e75d8
Grib <Grib@192.168.0.26>
2018-01-01 22:20:27 +0200

    Создание таблицы User + сзаимосвязь с таблицей Address

--- SQL/create-user-table.sql
+++ SQL/create-user-table.sql
@@ -1,11 +1,22 @@
 /**
  * Author:  Grib
- * Created: 01.01.2018:19.50
+ * Created: 01.01.2018:22.20
+ * Ver: 0.2
  */
 CREATE TABLE `user` 
 ( 
     `id` INT NULL AUTO_INCREMENT COMMENT 'id пользователя' , 
-    `name` CHAR(70) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'имя пользователя' , 
-    `password` CHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'пароль пользователя' , PRIMARY KEY (`id`), UNIQUE (`name`)
+    `id_address` INT NULL COMMENT 'связь с таблицей: USER' , 
+    `name` CHAR(50) NULL COMMENT 'имя пользователя' , 
+    `password` CHAR(50) NULL COMMENT 'пароль пользователя' , 
+    /*Ключи*/
+    PRIMARY KEY (`id`), 
+    INDEX (`id_address`), 
+    UNIQUE (`name`)
 )
-ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci COMMENT = 'user (таблийца пользователей)';
+ ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci COMMENT = 'user (таблийца пользователей)';
+
+ALTER TABLE `user` /*Связь таблиц User and Address по key (id_address User) и (id Address)*/
+    ADD FOREIGN KEY (`id_address`) 
+    REFERENCES `address`(`id`) 
+    ON DELETE CASCADE ON UPDATE CASCADE;
