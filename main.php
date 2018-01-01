fa336e467e6c2d619102c627fac0f744681ceace
Grib <Grib@192.168.0.26>
2018-01-01 22:53:01 +0200

    Создание страницы пльзователя

---
+++ main.php
@@ -0,0 +1,23 @@
+<?php
+
+/* 
+ * TiJester
+ * UA Odessa* 
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+    Добро подаловать: 
+<?php
+    echo "<b>".htmlentities($_GET["user"])."</b><br/>";
+?>
+        
+    </body>
+</html>
+
