b7931a15a760a84649a911ed058eb5219df2cad8
Grib <Grib@192.168.0.26>
2018-01-01 19:56:11 +0200

    Удаление процедурного подключения к БД

--- index.php
+++ index.php
@@ -1,24 +1,3 @@
-<?php // тестовое соединение
-// Загружаем соединение с базой данных
-require_once 'includes/class_db.php';
-
-// Соединение с базой данных
-class_db::ConnectDB(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-
-// Выполняет запрос к базе данных
-$result = mysql_query("SELECT VERSION() AS VERSION");
-
-// Обрабатывает ряд результата запроса и возвращает ассоциативный массив
-$row = mysql_fetch_assoc($result);
-
-// Выводит версию сервера MySQL
-echo "MySQL: ".$row['VERSION'];
-
-// Закрываем соединение с базой данных
-class_db::Close();
-// тестовое соединение
-?>
-
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
