9a9233c095149a5b486cc648e8775bec3b9bd35a
Grib <Grib@192.168.0.26>
2018-01-01 20:38:31 +0200

    Обновление таблицы user (пользователи) add адреса

--- SQL/update-user-table.sql
+++ SQL/update-user-table.sql
@@ -8,10 +8,10 @@
  */
 
 ALTER TABLE `user` 
-    ADD `address_country` INT(70) NOT NULL COMMENT 'адрес (страна)' AFTER `password`, ADD UNIQUE (`address_country`),
-    ADD `address_city` INT(70) NOT NULL COMMENT 'адрес (город)' AFTER `address_country`, ADD UNIQUE (`address_city`),
-    ADD `address_street` INT(100) NOT NULL COMMENT 'адрес (улица)' AFTER `address_city`, ADD UNIQUE (`address_street`),
-    ADD `address_street_num` INT(10) NOT NULL COMMENT 'адрес (номер по улице)' AFTER `address_street`, ADD UNIQUE (`address_street_num`),
-    ADD `address_apartment` INT(10) NOT NULL COMMENT 'адрес (номер квартиры)' AFTER `address_street_num`, ADD UNIQUE (`address_apartment`);
+    ADD `address_country` CHAR(70) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'адрес (страна)' AFTER `password`, ADD UNIQUE (`address_country`),
+    ADD `address_city` CHAR(70) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'адрес (город)' AFTER `address_country`, ADD UNIQUE (`address_city`),
+    ADD `address_street` CHAR(70) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'адрес (улица)' AFTER `address_city`, ADD UNIQUE (`address_street`),
+    ADD `address_street_num` CHAR(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'адрес (номер по улице)' AFTER `address_street`, ADD UNIQUE (`address_street_num`),
+    ADD `address_apartment` CHAR(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'адрес (номер квартиры)' AFTER `address_street_num`, ADD UNIQUE (`address_apartment`);
 
 
