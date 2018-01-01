abf2be1c09977a20f83e92ca5bdce2f27e03cd38
Grib <Grib@192.168.0.26>
2018-01-02 01:53:39 +0200

    Правка запроса для таблицы User

--- SQL/create-user-table.sql
+++ SQL/create-user-table.sql
@@ -1,22 +1,15 @@
 /**
  * Author:  Grib
- * Created: 01.01.2018:22.20
- * Ver: 0.2
+ * Created: 02.01.2018:1.49
+ * Ver: 0.3
  */
 CREATE TABLE `user` 
 ( 
     `id` INT NULL AUTO_INCREMENT COMMENT 'id пользователя' , 
-    `id_address` INT NULL COMMENT 'связь с таблицей: USER' , 
     `name` CHAR(50) NULL COMMENT 'имя пользователя' , 
     `password` CHAR(50) NULL COMMENT 'пароль пользователя' , 
     /*Ключи*/
     PRIMARY KEY (`id`), 
-    INDEX (`id_address`), 
     UNIQUE (`name`)
 )
  ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci COMMENT = 'user (таблийца пользователей)';
-
-ALTER TABLE `user` /*Связь таблиц User and Address по key (id_address User) и (id Address)*/
-    ADD FOREIGN KEY (`id_address`) 
-    REFERENCES `address`(`id`) 
-    ON DELETE CASCADE ON UPDATE CASCADE;
