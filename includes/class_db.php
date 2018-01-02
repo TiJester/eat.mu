accaaee4d3d035d5abfa7519dbba3996aabd7a79
Grib <Grib@192.168.0.26>
2018-01-02 17:37:38 +0200

    Создание конструктора для вызова метода класса MySqli

--- includes/class_db.php
+++ includes/class_db.php
@@ -21,4 +21,35 @@ class class_db extends mysqli{
     private $pass = "";
     private $dbName = "eat_test_01012017";
     private $dbHost = "localhost";
+    
+    //Этот метод должен быть статическим и должен возвращать экземпляр объекта, если объект еще не существует.
+    public static function getInstance()
+    {
+        if (!self::$instance instanceof self)
+        {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+    
+    //Методы клонирования и пробуждения предотвращают внешнюю копию экземпляров класса Singleton, исключая, таким образом, возможность дублирования объектов.
+    public function __clone() 
+    {
+        trigger_error('Clone (Клонирование) не допускаеться', E_USER_ERROR);
+    }
+    public function __wakeup()
+    {
+        trigger_error('Дезерелизация не допускаеться', E_USER_ERROR);
+    }
+
+    //частный конструктор
+    private function __construct() 
+    { //соеденение с БД
+        parent::__construct($this->dbHost, $this->dbName, $this->user, $this->pass);
+        if (mysqli_connect_error())
+        {
+            exit('Ошибка соединения ('.mysqli_connect_errno().')'.mysqli_connect_error());
+        }
+        parent::set_charset('utf-8');
+    }
 }
