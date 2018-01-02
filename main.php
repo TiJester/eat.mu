005b5adf1a3a2ea70803a7aa26df9ae613a672d2
Grib <Grib@192.168.0.26>
2018-01-02 23:16:22 +0200

    Замена кода на запросы из класса

--- main.php
+++ main.php
@@ -1,9 +1,9 @@
 <?php
-
 /* 
  * TiJester
  * UA Odessa* 
  */
+require_once ("includes/class_db.php");
 ?>
 
 <!DOCTYPE html>
@@ -17,37 +17,41 @@
 <?php
     echo "<b>".htmlentities($_GET["user"])."</b><br/>";
     
-    //подключение к базе данных
-    $con = mysqli_connect("localhost", "root", "");
-    if (!$con) {
-    exit('Connect Error (' . mysqli_connect_errno() . ') '
-    . mysqli_connect_error());
-    }
-    else 
+//    //подключение к базе данных
+//    $con = mysqli_connect("localhost", "root", "");
+//    if (!$con) {
+//    exit('Connect Error (' . mysqli_connect_errno() . ') '
+//    . mysqli_connect_error());
+//    }
+//
+//    
+//    // установить набор символов клиента по умолчанию
+//    mysqli_set_charset($con, 'utf-8');
+//    
+//    //соеденение с БД
+//    mysqli_select_db($con, "eat_test_01012017");
+//    $user = mysqli_real_escape_string($con, htmlentities($_GET["user"]));
+//    $user_table = mysqli_query($con, "SELECT id FROM user WHERE name='".$user."'");
+//    if (mysqli_num_rows($user_table)<1)
+//    {
+//        exit("Пользователь не найден: ". htmlentities($_GET["user"]) ." Проверте орфографию и поробуйте еще раз.");
+//    }
+//    $row = mysqli_fetch_row($user_table);
+//    $userID = $row[0];
+//    mysqli_free_result($user_table);
+    $userID = class_db::getInstance()->get_user_id_by_name($_GET["user"]);
+    if (!$userID)
     {
-        echo "Подключение прошло!";    
+        exit("<br/>Пользователь не найден: <b>" .$_GET["user"]."</b> Проверте орфографию и поробуйте еще раз.<br/>");
+//        exit("Пользователь не найден: ". htmlentities($_GET["user"]) ." Проверте орфографию и поробуйте еще раз.");
     }
-    
-    // установить набор символов клиента по умолчанию
-    mysqli_set_charset($con, 'utf-8');
-    
-    //соеденение с БД
-    mysqli_select_db($con, "eat_test_01012017");
-    $user = mysqli_real_escape_string($con, htmlentities($_GET["user"]));
-    $user_table = mysqli_query($con, "SELECT id FROM user WHERE name='".$user."'");
-    if (mysqli_num_rows($user_table)<1)
-    {
-        exit("Пользователь не найден: ". htmlentities($_GET["user"]) ." Проверте орфографию и поробуйте еще раз.");
-    }
-    $row = mysqli_fetch_row($user_table);
-    $userID = $row[0];
-    mysqli_free_result($user_table);
 ?>
     
     <table border="black"> 
         <tr> <th>Адрес</th></tr>
 <?php
-    $result = mysqli_query($con, "SELECT address_country, address_city, address_street, address_street_num, address_apartment FROM address WHERE id_user =".$userID);
+//    $result = mysqli_query($con, "SELECT address_country, address_city, address_street, address_street_num, address_apartment FROM address WHERE id_user =".$userID);
+    $result = class_db::getInstance()->get_user_by_users_id($userID);
     while ($row = mysqli_fetch_array($result)) 
     {
         echo "<td>".htmlentities($row["address_country"]).", ";
@@ -57,7 +61,7 @@
         echo htmlentities($row["address_apartment"])."</td>";
     }
     mysqli_free_result($result);
-    mysqli_close($con);
+//    mysqli_close($con);
 ?>
     </table>
     </body>
