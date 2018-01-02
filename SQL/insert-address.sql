0d9eedc18a65869bce3184d541a4485813687d9b
Grib <Grib@192.168.0.26>
2018-01-02 09:12:11 +0200

    Внесенеи правок в добовление демо данных в талбицу address

--- SQL/insert-address.sql
+++ SQL/insert-address.sql
@@ -10,5 +10,5 @@
 INSERT INTO `address` 
     (`id`, `id_user`, `address_country`, `address_city`, `address_street`, `address_street_num`, `address_apartment`) 
     VALUES 
-    (NULL, '1', 'Украина', 'Одесса', 'Аграрная', '4', ''), 
+    (NULL, '1', 'Украина', 'Одесса', 'Аграрная', '4', '0'),/*address_apartment - Не дает внести пустые значения*/ 
     (NULL, '2', 'Украина', 'Одесса', 'Добровольского', '78', '43')
