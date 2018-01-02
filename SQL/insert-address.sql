125bb16c4760131d61f50602bfcb9ac5ede9a5af
Grib <Grib@192.168.0.26>
2018-01-02 09:01:53 +0200

    Внесенее демо данных в таблицы User Address

---
+++ SQL/insert-address.sql
@@ -0,0 +1,14 @@
+/* 
+ * TiJester
+ * UA Odessa  * 
+ */
+/**
+ * Author:  Grib
+ * Created: 02.01.2018:09.00
+ */
+
+INSERT INTO `address` 
+    (`id`, `id_user`, `address_country`, `address_city`, `address_street`, `address_street_num`, `address_apartment`) 
+    VALUES 
+    (NULL, '1', 'Украина', 'Одесса', 'Аграрная', '4', ''), 
+    (NULL, '2', 'Украина', 'Одесса', 'Добровольского', '78', '43')
---
+++ SQL/insert-user.sql
@@ -0,0 +1,15 @@
+/* 
+ * TiJester
+ * UA Odessa  * 
+ */
+/**
+ * Author:  Grib
+ * Created: 02.01.2018:8:56
+ */
+
+INSERT INTO `user` 
+    (`id`, `name`, `password`) 
+    VALUES 
+    (NULL, 'user', 'user'), 
+    (NULL, 'grib', '123123')
+
