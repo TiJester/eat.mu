eccf4958f25de4425e597d53a3356916b1800655
Grib <Grib@192.168.0.26>
2018-01-02 23:16:51 +0200

    Добовления новых функций в класс

--- includes/class_db.php
+++ includes/class_db.php
@@ -45,11 +45,40 @@ class class_db extends mysqli{
     //частный конструктор
     private function __construct() 
     { //соеденение с БД
-        parent::__construct($this->dbHost, $this->dbName, $this->user, $this->pass);
+        parent::__construct($this->dbHost, $this->user, $this->pass, $this->dbName);
         if (mysqli_connect_error())
         {
             exit('Ошибка соединения ('.mysqli_connect_errno().')'.mysqli_connect_error());
         }
         parent::set_charset('utf-8');
+    }
+    
+    //Функция get_user_id_by_name, Функция требует имя пользователя в качестве входного параметра и возвращает идентификатор  id user.
+    public function get_user_id_by_name($name) 
+    {
+    $name = $this->real_escape_string($name);
+    $users = $this->query("SELECT id FORM user WHERE name = '".$name."'");
+    if ($users->num_rows > 0)
+        {
+        $row = $users->fetch_row();
+        return $row[0];
+        }
+    else {
+        return NULL;
+        }
+    }
+    
+    // Функция get_user_by_users_id. Функция требует идентификатор. Идентификатор в качестве входного параметра и возвращает адрес, зарегистрированные для пользователя.
+    public function get_user_by_users_id($userID)
+    {
+        return $this->query("SELECT address_country, address_city, address_street, address_street_num, address_apartment FROM address WHERE id_user =".$userID);
+    }
+    
+    // Функция create_user. Функция создает новую запись в таблице user. Функция требует имя и пароль нового user (пользователя) в качестве входных параметров и не возвращает никаких данных.
+    public function create_user($name, $password)
+    {
+        $name = $this-> real_escape_string($name);
+        $password = $this->real_escape_string($password);
+        $this->query("INSERT INTO user (name, password) VALUES ('" . $name . "','". $password . "')");
     }
 }
