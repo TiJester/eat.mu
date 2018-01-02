ae21ec215bafa38dd6b86790a35a5d7c368ab696
Grib <Grib@192.168.0.26>
2018-01-02 23:19:09 +0200

    Добовление нового пользователя

--- createNewUser.php
+++ createNewUser.php
@@ -4,11 +4,12 @@ TiJester
 UA Odessa 
 -->
 <?php
+require_once ("includes/class_db.php");
     /*Учетные данные подключения к базе данных*/
-$dbHost="localhost"; //on MySql
-$dbXeHost="localhost/XE";
-$dbUsername="root";
-$dbPassword="";
+//$dbHost="localhost"; //on MySql
+//$dbXeHost="localhost/XE";
+//$dbUsername="root";
+//$dbPassword="";
 
     /** другие переменные */
 $userNameIsUnique = true; //проверяем есть ли вводимый пользователь в базе данных
@@ -27,22 +28,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     }
     
     // создание соеденения с БД
-    $con = mysqli_connect($dbHost, $dbUsername,$dbPassword);
-    if (!$con)
-    {
-        exit("ошибка соединения (".mysqli_connect_errno().")".mysqli_connect_error());
-    }
-    //устанавливаем набор символов по умолчанию
-    mysqli_set_charset($con, 'utf-8');
+//    $con = mysqli_connect($dbHost, $dbUsername,$dbPassword);
+//    if (!$con)
+//    {
+//        exit("ошибка соединения (".mysqli_connect_errno().")".mysqli_connect_error());
+//    }
+//    //устанавливаем набор символов по умолчанию
+//    mysqli_set_charset($con, 'utf-8');
     
-    // Убедитесь, что пользователь, чье имя совпадает с полем пользователя, уже существует 
-    mysqli_select_db($con, "eat_test_01012017");
-    $user = mysqli_real_escape_string($con, $_POST["user"]);
-    $users = mysqli_query($con, "SELECT id FROM user WHERE name='".$user."'");
-    $usersIDnum = mysqli_num_rows($users);
-    if ($usersIDnum) //есть ли данный пользоыватель в БД.
+//    // Убедитесь, что пользователь, чье имя совпадает с полем пользователя, уже существует 
+//    mysqli_select_db($con, "eat_test_01012017");
+//    $user = mysqli_real_escape_string($con, $_POST["user"]);
+//    $users = mysqli_query($con, "SELECT id FROM user WHERE name='".$user."'");
+//    $usersIDnum = mysqli_num_rows($users);
+//    if ($usersIDnum) //есть ли данный пользоыватель в БД.
+//    {
+//        $userNameIsUnique = false; 
+//    }
+    
+    $userID = class_db::getInstance()->get_user_id_by_name($_POST["user"]);
+    if ($userID)
     {
-        $userNameIsUnique = false; 
+        $userNameIsUnique = FALSE;
     }
     
     //проверяем введены ли данные паролей и совпадают ли они
@@ -62,13 +69,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
      * Если данные были успешно проверены, добавьте их как новую запись в базу данных «user». 
      * После добавления новой записи закройте соединение и перенаправьте приложение на editUserList.php. * /
      */
+//    if (!$userIsEmpty && $userNameIsUnique && !$passwordIsEmpty && !$password2IsEmpty && $passwordIsValid)
+//    {
+//        $password = mysqli_real_escape_string($con, $_POST['password']);
+//        mysqli_select_db($con, "eat_test_01012017");
+//        mysqli_query($con, "INSERT user (name, password) VALUES ('". $user ."','".$password."')");
+//        mysqli_free_result($users);
+//        mysqli_close($con);
+//        header('Location: editUserList.php');
+//        exit;
+//    }
     if (!$userIsEmpty && $userNameIsUnique && !$passwordIsEmpty && !$password2IsEmpty && $passwordIsValid)
     {
-        $password = mysqli_real_escape_string($con, $_POST['password']);
-        mysqli_select_db($con, "eat_test_01012017");
-        mysqli_query($con, "INSERT user (name, password) VALUES ('". $user ."','".$password."')");
-        mysqli_free_result($users);
-        mysqli_close($con);
+        class_db::getInstance()->create_user($_POST["user"], $_POST["password"]);
         header('Location: editUserList.php');
         exit;
     }
@@ -82,7 +95,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     </head>
     <body>
         Добро пожаловать!<br>
-    <form action="createNewAddress.php" method="POST">
+    <form action="createNewUser.php" method="POST">
         Ваше имя: <input type="text" name="user" value="" size="50" /> <br />
     <?php
     if ($userIsEmpty)
