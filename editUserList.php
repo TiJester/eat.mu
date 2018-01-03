1a5961ad5059dcbb9a3a77c7cfb49c6f0e61225a
Grib <Grib@192.168.0.26>
2018-01-03 13:10:30 +0200

    Старт сесии

--- editUserList.php
+++ editUserList.php
@@ -1,9 +1,14 @@
 <?php
-
 /* 
  * TiJester
  * UA Odessa  * 
  */
-
-echo "Hi!";
+    session_start();
+    if (array_key_exists("user", $_SESSION)){
+        echo "Привет сесия: " . $_SESSION['user'];
+    }
+    else {
+        header('Location: index.php');
+        exit;
+    }
 
