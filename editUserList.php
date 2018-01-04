85e571f53bd5007b10fe1382d011408f744f7160
Grib <Grib@192.168.0.26>
2018-01-04 21:51:28 +0200

    Добовление формы: добавить адрес

--- editUserList.php
+++ editUserList.php
@@ -11,4 +11,18 @@
         header('Location: index.php');
         exit;
     }
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//RU">
+
+<html>
+    <head>
+    <meta http-equiv = "Content-Type" content = "text/html; charset=UTF-8" />      
+    </head>
+    <body>
+        <form name="addNewAddress" action="editUser.php">
+            <input type="submit" value="" name="Добавить" />
+        </form>
+    </body>
+    
+</html>
 
