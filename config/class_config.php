b16ac0930b801fdd145ef899007b8fcb28dcc0fe
admin <admin@reserves.ef.local>
2018-03-29 16:24:30 +0300

    Конфигурация классов CMS V 0.1

---
+++ config/class_config.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * TiJester
+ * UA Odessa 
+ */
+
+/**
+ * 2018(С) Шевченко Г.Ю.
+ * Конфигурация классов CMS
+ * V 0.1
+ * 11 включений
+ */
+
+    //  Выставляем уровень обработки ошибок
+    error_reporting(E_ALL & ~E_NOTICE);
+    
+    require_once ("class/base_field.php"); //   Базовый класс
+    require_once ("class/base_field_text.php"); //  Класс текстового поля
+    require_once ("class/base_field_text_email.php"); //    Класс корректности поля email
+    require_once ("class/base_field_text_english.php"); //  Класс проверки корреткности на латиницу
+    require_once ("class/base_field_text_int.php"); //  Класс проверки на целочисленное значение
+    require_once ("class/base_field_textarea.php"); //  Класс текстовой обости textarea
+    require_once ("class/base_field_password.php"); //   Класс текстового поля со скрытым значением
+    
+    require_once ("class/form.php"); // Класс формирование формы
+    
+    require_once ("class/ExceptionMember.php"); //  Исключения к несцществующему элементу
+    require_once ("class/ExceptionMySql.php"); //  Исключения Запросов
+    require_once ("class/ExceptionObject.php"); //  Исключения объектов
