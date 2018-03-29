eec8d0cf4be2399e9499ee38bbffbfe9adf322c7
admin <admin@reserves.ef.local>
2018-03-29 12:51:01 +0300

    SQL запрос на добовление таблицы USERS v 0.2

--- SQL/users.sql
+++ SQL/users.sql
@@ -4,14 +4,17 @@
  */
 /**
  * Author:  admin
- * Created: 19.03.2018
- * v 0.1 
+ * Created: 29.03.2018
+ * users
+ * v 0.2 
  */
 
 CREATE TABLE users (
 id_user INT(10) NOT NULL AUTO_INCREMENT,
 name TINYTEXT NOT NULL,
 pass TINYTEXT NOT NULL,
+email TINYTEXT NOT NULL,    # v 0.2
+description TEXT NOT NULL,  # v 0.2
 add_date DATETIME NOT NULL,
 PRIMARY KEY (id_user)
 )
