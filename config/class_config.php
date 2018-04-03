07973d87096e6a5ed37136248949d8648b94c542
admin <admin@reserves.ef.local>
2018-04-03 13:45:14 +0300

    Конфигурация классов CMS V 0.1.1 13 включений

--- config/class_config.php
+++ config/class_config.php
@@ -8,8 +8,8 @@
 /**
  * 2018(С) Шевченко Г.Ю.
  * Конфигурация классов CMS
- * V 0.1
- * 11 включений
+ * V 0.1.1
+ * 13 включений
  */
 
     //  Выставляем уровень обработки ошибок
@@ -21,7 +21,9 @@
     require_once ("class/base_field_text_english.php"); //  Класс проверки корреткности на латиницу
     require_once ("class/base_field_text_int.php"); //  Класс проверки на целочисленное значение
     require_once ("class/base_field_textarea.php"); //  Класс текстовой обости textarea
-    require_once ("class/base_field_password.php"); //   Класс текстового поля со скрытым значением
+    require_once ("class/base_field_password.php"); //  Класс текстового поля со скрытым значением
+    require_once ("class/base_field_hidden.php"); //    Класс скрытого поля
+    require_once ("class/base_field_hidden_int.php"); //    Класс скрытого поля с целочисленым значением
     
     require_once ("class/form.php"); // Класс формирование формы
     
