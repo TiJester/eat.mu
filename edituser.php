cc5f1fe861065b408239b36eb93afcf2cd2461a3
admin <admin@reserves.ef.local>
2018-04-04 16:36:10 +0300

    Страница редактирования пользователя V 0.1

--- edituser.php
+++ edituser.php
@@ -12,6 +12,7 @@
  */
 
 //
+echo 'test<br>';
     try {
         //  Подключам все необходимые классы
         require_once ("config/class_config.php");
@@ -25,7 +26,7 @@
             
             $query = "SELECT * FORM users WHERE id_user = $_GET[id_user]";
             $result = mysqli_query($link, $query);
-            if(!$result){
+            if(!$link){
                 throw new ExceptionMySql(mysqli_error($link));
             }
             $_REQUEST = mysqli_fetch_array($result); 
@@ -71,16 +72,46 @@
         ///////////////////////////
         if(!empty($_POST))
         {
+            //  Устанавливаем соеденение с базой данных
+            require_once ("config/config.php");
+            //  Проверяем корректность заполнения HTML-формы и обрабатываем текстовые поля
+            $error = $form->check();
             
+            //  Проверяем, равны ли пароли
+            if($form->fields['pass']->value != $form->fields['pass2']->value){
+                $error[] = "Пароли не равны";
+            }
+            //  Проверка на совпадения email с ранее зарегистрироваными пользователями
+            $query = "SELECT COUNT(*) FROM users WHERE email ='{$form->fields[email]->value}'";
+            $result = mysqli_query($link, $query);
+            if(!$result){
+                throw new ExceptionMySql(mysqli_error($link));
+            }
+            //  Обновляем запись пользователя
+            $query = "UPDATE users SET pass = MD5('{$form->fields[pass]->value}'), email = '{$form->fields[email]->value}, description = '{$form->fields[description]->value}' WHERE id_user = {$form->fields[id_user]->value}";
+            if(!mysqli_query($link, $query)){
+                throw new ExceptionMySql(mysqli_error($link));
+            }
+            
+            //  Перегружаем страницу для сброса POST данных
+            header("Location:user_list.php");
+            exit();
         }
         
         //  3.Видимая часть страницы
         ////////////////////////////
         //  Выводим сообщение об ошибках если они имеються
-        
+        if(!empty($error)){
+            foreach ($error as $err){
+                echo "<span style=\"color:red\">$err</span><br>";
+            }
+        }
         //  Выводим HTML-форму
         $form->print_form();
         
-    } catch (Exception $ex) {
-
+    } catch (ExceptionMySql $exc) {
+        echo "<p class=help> Произошла исключительная систуация <b> ExceptionMySql</b> при обращении к MySql</p>";
+        echo "<p class=help> {$exc->getMySQLError()}<br>". /*n12br*/($exc->getSQLQuery())."</p>";  
+        echo "<p class=help> Ошибка в файле{$exc->getFile()} в строке {$exc->getLine()}.</p>";
+        exit();
     }
