8314f6af76fad956909223958ba415010a822195
admin <admin@reserves.ef.local>
2018-04-05 15:52:54 +0300

    Страница редактирования пользователя V 0.1.1

--- edituser.php
+++ edituser.php
@@ -8,7 +8,7 @@
 /**
  * 2018(С) Шевченко Г.Ю.
  * Страница редактирования пользователя
- * V 0.1
+ * V 0.1.1
  */
 
 //
@@ -19,18 +19,18 @@ echo 'test<br>';
         
         //  Если это первое обращение к HTML-форме извлекаем информацию из таблицы users
         if(empty($_POST)){
-            $_GET['id_user'] = intval($GET['id_user']);
+            $_GET['id_user'] = intval($_GET['id_user']);
             
             //  1.Устанавливаем соединение с базой данных
             require_once ("config/config.php");
             
-            $query = "SELECT * FORM users WHERE id_user = $_GET[id_user]";
+            $query = "SELECT * FROM users WHERE id_user = $_GET[id_user]";
             $result = mysqli_query($link, $query);
             if(!$link){
                 throw new ExceptionMySql(mysqli_error($link));
             }
             $_REQUEST = mysqli_fetch_array($result); 
-            $_REQUEST["pass"]=$_REQUEST["pass2"]; //    $_REQUEST Переменные HTTP-запроса
+            $_REQUEST["pass"] = $_REQUEST["pass2"]; //    $_REQUEST Переменные HTTP-запроса
         }
         
         //  Формиование формы
@@ -87,15 +87,26 @@ echo 'test<br>';
             if(!$result){
                 throw new ExceptionMySql(mysqli_error($link));
             }
+            if(mysqli_fetch_assoc($result)){
+                $error[] = "Пользователь с электронным адресом: <br><b><i>{$form->fields[email]->value}</i></b><br> уже существует";
+            }
+            
+            //  Подгружаем информацию о себе из базы данных
+            $query = "SELECT COUNT(*) FROM users WHERE description ='{$form->fields[description]->value}'";
+            $result = mysqli_query($link, $query);
+            if(!$result){
+                throw new ExceptionMySql(mysqli_error($link));
+            }
+            
             //  Обновляем запись пользователя
-            $query = "UPDATE users SET pass = MD5('{$form->fields[pass]->value}'), email = '{$form->fields[email]->value}, description = '{$form->fields[description]->value}' WHERE id_user = {$form->fields[id_user]->value}";
+            $query = "UPDATE users SET pass = MD5('{$form->fields[pass]->value}'), email = '{$form->fields[email]->value}', description = '{$form->fields[description]->value}' WHERE id_user = '{$form->fields[id_user]->value}'";
             if(!mysqli_query($link, $query)){
                 throw new ExceptionMySql(mysqli_error($link));
             }
             
             //  Перегружаем страницу для сброса POST данных
-            header("Location:user_list.php");
-            exit();
+            //header("Location:user_list.php");
+            //exit();
         }
         
         //  3.Видимая часть страницы
@@ -108,7 +119,7 @@ echo 'test<br>';
         }
         //  Выводим HTML-форму
         $form->print_form();
-        
+          
     } catch (ExceptionMySql $exc) {
         echo "<p class=help> Произошла исключительная систуация <b> ExceptionMySql</b> при обращении к MySql</p>";
         echo "<p class=help> {$exc->getMySQLError()}<br>". /*n12br*/($exc->getSQLQuery())."</p>";  
