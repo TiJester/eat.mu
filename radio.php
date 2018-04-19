831de2a2ea617593e7ad40fd67fc16a9046415ea
admin <admin@reserves.ef.local>
2018-04-19 15:55:11 +0300

    Тест радио-конпки

---
+++ radio.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--
+TiJester
+UA Odessa 
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // Тест радио-конпки
+        //  Подключаем все необходимые классы
+        require_once("config/class_config.php");
+        
+        $one = new base_field_radio(
+                "one",
+                "Вертикальный вариант",
+                array("Да",
+                    "Нет"),
+                1);
+        $two = new base_field_radio(
+                "two", 
+                "Горизонтальный вариант",
+                array("Да",
+                    "Нет"),
+                1,
+                "horizontal");
+        
+        $form = new form(array(
+            "one" => $one,
+            "two" => $two),
+            "Вперед",
+            $class_name);
+        
+    $form->print_form();   
+        ?>
+    </body>
+</html>
