b50b793bc6c4537617b107ab42573742713154a0
admin <admin@reserves.ef.local>
2018-04-24 11:00:12 +0300

    Описание класа base_field_title Заголовок V 0.1.1

--- class/base_field_title.php
+++ class/base_field_title.php
@@ -9,7 +9,7 @@
  * 2018(С) Шевченко Г.Ю.
  * Описание класа base_field_title
  * Заголовок
- * V 0.1
+ * V 0.1.1
  */
 
 //  Выставляем уровень обработки ошибок
@@ -46,7 +46,19 @@ class base_field_title extends base_field{
     function get_html() {
         //  Формируем тэг
         $tag = htmlspecialchars($this->value, ENT_QUOTES);
-        
+        $pattern = "#\[b\](.+)\[\/b\]#isU";
+        $tag = preg_replace($pattern, '<b>\\1</b>',$tag);
+        $pattern = "#\[i\](.+)\[\/i\]#isU";
+        $tag = preg_replace($pattern, '<i>\\1</i>' ,$tag);
+        $pattern = "#[url\][\s]*((?=http:)[\S]*)[\s]*\[\/url\]#si";
+        $tag = preg_replace($pattern, '<a href="\\1" target=_blank>\\1</a>', $tag);
+        $pattern = "#\[url[\s]*=[\s]*((?=http:)[\S]+)[\s]*\][\s]*([^\[]*)\[/url\]#isU";
+        $tag = preg_replace($pattern, '<a href="\\1" target=_blank>\\2</a>' ,$tag);
+        if(get_magic_quotes_gpc()){
+            $tag = stripcslashes($tag); // может строку 60 сюда!
+        }
+        $tag = "<h".$this->h_type.">".$this->value."</h>".$this->h_type.">";
+        return array($this->caption, $tag);
     }    
     //  Метод проверяющий корректность переданых данных
     function check(){
