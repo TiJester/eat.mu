bc220850ce5b0bd56c3ebe550bc31d6c7504ae0b
admin <admin@reserves.ef.local>
2018-05-02 14:34:35 +0300

    Описание класа pager Абстрактный класс постраничной навигации __toString() Альтернативный вывод навигации print_page() V 0.2.0

--- class/pager.php
+++ class/pager.php
@@ -8,8 +8,9 @@
 /**
  * 2018(С) Шевченко Г.Ю.
  * Описание класа pager
- * Абстрактный класс постраничной навигации
- * V 0.1.1
+ * Абстрактный класс постраничной навигации __toString()
+ * Альтернативный вывод навигации print_page()
+ * V 0.2.0
  */
 
 //  Выставляем уровень обработки ошибок
@@ -92,5 +93,64 @@ abstract class pager {
     }
     
     //  Альтернативный вид постраничной навигации
-    
+    public function print_page(){
+        //  Строка для возвращаемого результата
+        $return_page = "";
+        
+        //  Через GET-параметр page передаеться номер текущей страницы
+        $page = intval($_GET['page']);
+        if(empty($page)) {
+            $page = 1;
+        }
+        
+        //  Вычисляем число страниц в системе
+        $number = (int)($this->get_total()/$this->get_pnumber());
+        if((float)($this->get_total()/$this->get_pnumber()) - $number !=0){
+            $number++;
+        }
+        
+        //  Ссылка на первую страницу
+        $return_page .= "<a href='$_SERVER[PHP_SELF]?page=1{$this->get_parameters()}' class=main_txt_lnk>&lt;&lt;</a> ... ";
+        
+        //  Выводим ссылку назад если это не первая страница
+        if($page != 1){
+            $return_page .= "<a href='$_SERVER[PHP_SELF]?page=".($page - 1)."($this->get_parameters())' class=main_txt_lnk>&lt;</a> ...";
+        }
+        
+        //  Выводим предыдущие элементы
+        if($page > $this->get_page_link() + 1){
+            for($i = $page - $this->get_page_link(); $i < $page; $i++){
+                $return_page .= "<a href='$_SERVER[PHP_SELF]?page=$i'class=main_txt_lnk>$i</a>";
+            }
+        }
+        else {
+            for($i = 1; $i < $page; $i++){
+                $return_page .= "<a href='$_SERVER[PHP_SELF]?page=$i' class=main_txt_lnk>$i</a>";
+            }
+        }
+        
+        //  Выводим текущий элемент
+        $return_page .= "$i";
+        //  Выводим селедующие элементы
+        if($page + $this->get_page_link() < $number){
+            for($i = $page + 1; $i <= $page + $this->get_page_link(); $i++){
+                $return_page .= "<a href='$_SERVER[PHP_SELF]?page=$i' class=main_txt_lnk>$i</a>";
+            }
+        }
+        else {
+            for($i = $page + 1; $i <= $number; $i++){
+                $return_page .= "<a href='$_SERVER[PHP_SELF]?page=$i' class=main_txt_lnk>$i</a>";
+            }
+        }
+        
+        //  Выводим ссылку вперед если это не последняя страница
+        if($page != $number){
+            $return_page .= " ... <a href='$_SERVER[PHP_SELF]?page=".($page + 1). "{$this->get_parameters()}' class=main_txt_lnk>&gt;</a>";
+        }
+        
+        //  Ссылка на соследную страницу
+        $return_page .= " ... <a href='$_SERVER[PHP_SELF]?page=$number{$this->get_parameters()}' class=main_txt_lnk>&gt;&gt;</a>";
+        
+        return $return_page;
+    }
 }
