9fd646e945113050e4f161bae8ec95e8147978e1
admin <admin@reserves.ef.local>
2018-05-02 16:34:45 +0300

    Описание класа pager_file Файловая постраничная навигация V 0.1.0

--- class/pager_file.php
+++ class/pager_file.php
@@ -67,5 +67,41 @@ class pager_file extends pager{
     }
     
     //  Возвращает массив строк файла по номеру страницы $index
-    
+    public function get_page(){
+        //  Текущая страница
+        $page = $_GET['page'];
+        if(empty($page)){
+            $page = 1;
+        }
+        //  Колличество записей в файле
+        $total = $this->get_total();
+        //  Вычисляем число страниц в системе
+        $number = (int)($total/$this->get_pnumber());
+        if((float)($total/$this->get_pnumber()) - $number != 0){
+            $number++;
+        }
+        //  проверяем попадает ли запрошеный номер в интервал от 1 до get_total()
+        if($page <=0 || $page > $number){
+            return 0;
+        }
+        //  Извлекаем позицию текущей страницы
+        $arr = array();
+        $fd = fopen($this->filename, "r");
+        if(!$fd){
+            return 0;
+        }
+        //  Номер, ачиная с которого следует выбрать строки файла
+        $first =($page - 1)*$this->get_pnumber();
+        for($i =0; $i < $total; $i++){
+            $str = fgets($fd, 10000);
+            //  Пока не достигнут номер $first досрочно заканчиваем итерацию
+            if($i < $first) continue;
+            //  Если достигнут конец выборки досрочно покидаем цикл
+            if($i > $first + $this->get_pnumber() - 1) break;
+            //Помещаем строки в массив который будет возвращен методом
+            $arr[] = $str;
+        }
+        fclose($fd);
+        return $arr;
+    }
 }
