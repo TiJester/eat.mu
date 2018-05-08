220e63e8bb1cb630b0d6433d5e342332106e5b5a
Grib <Grib@192.168.0.26>
2018-05-09 00:31:50 +0300

    Страница постраничной навигации в файле V 0.1.2

--- pagers_file.php
+++ pagers_file.php
@@ -8,16 +8,17 @@
 /**
  * 2018(С) Шевченко Г.Ю.
  * Страница постраничной навигации в файле
- * V 0.1.1
+ * V 0.1.2
  */
 
     require_once ("config/class_config.php");
     //  Объявляем объект постраничной навигации
-    $obj = new pager_file("words.txt", 25, 5);
+    $obj = new pager_file("words_test.txt", 25, 4);
     //  Выводим ссылки на другие страницы
     echo $obj;
     
     //  Выводим содержимое текущей страницы
+    echo "<br>";
     $arr = $obj->get_page();
     for($i = 0; $i < count($arr); $i++){
         echo "{$arr[$i]}<br>";
