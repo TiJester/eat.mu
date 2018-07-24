f4a08d66a9c604c78b9b12b790e73c06a62e86c0
admin <admin@reserves.ef.local>
2018-07-24 15:29:34 +0300

    Страница постраничной навигации в деректории изображения V 0.1.1

--- pagers_dir_imag.php
+++ pagers_dir_imag.php
@@ -8,23 +8,20 @@
 /**
  * 2018(С) Шевченко Г.Ю.
  * Страница постраничной навигации в деректории изображения
- * V 0.1.0
+ * V 0.1.1
  */
 
-//  Выставляем уровень обработки ошибок
-error_reporting(E_ALL & ~E_NOTICE);
-
-    require_once ("config/class_config.php");
-
+    require_once("config/class_config.php");
 
     //  Объявляем объект постраничной навигации
-    $obj = new pager_dir("photo", 3, 2);
+    $obj = new pager_dir("photo", 3);
+    
     //  Выводим содержимое текущей страницы
     $arr = $obj->get_page();
     for($i=0; $i<count($arr); $i++){
         echo "<img src={$arr[$i]}&nbsp;&nbsp;&nbsp;>";
     }
     echo "<br>";
-    
+        
     //  Выводим ссылки на другие страницы
-    echo $obj;
+    echo $obj->print_page();
