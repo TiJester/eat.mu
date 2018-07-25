3d8db15557527ef64e78bc7d22847d82ea2b6d63
admin <admin@reserves.ef.local>
2018-07-25 15:03:45 +0300

    position v 0.1

---
+++ SQL/position.sql
@@ -0,0 +1,17 @@
+/* 
+ * TiJester
+ * UA Odessa  * 
+ */
+/**
+ * Author:  admin
+ * Created: 25.07.2018
+ * position
+ * v 0.1
+ */
+
+CREATE TABLE position(
+id_position INT(11) NOT NULL AUTO_INCREMENT,
+name TEXT NOT NULL,
+PRIMARY KEY (id_position)
+);
+
