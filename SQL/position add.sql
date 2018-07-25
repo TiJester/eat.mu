d2c2dfa5cb5b2af5321d5849181f5dc2f97460dd
admin <admin@reserves.ef.local>
2018-07-25 15:31:33 +0300

    position v 0.2

---
+++ SQL/position add.sql
@@ -0,0 +1,15 @@
+/* 
+ * TiJester
+ * UA Odessa  * 
+ */
+/**
+ * Author:  admin
+ * Created: 25.07.2018
+ * position add
+ * v 0.1
+ */
+
+INSERT INTO positions VALUES (NULL, 'Горячие блюда', '2018-07-25');
+INSERT INTO positions VALUES (NULL, 'Холодные блюда', '2018-07-25');
+INSERT INTO positions VALUES (NULL, 'Дисерт', '2018-07-25');
+INSERT INTO positions VALUES (NULL, 'Выпечка', '2018-07-25');
--- SQL/position.sql
+++ SQL/position.sql
@@ -6,12 +6,12 @@
  * Author:  admin
  * Created: 25.07.2018
  * position
- * v 0.1
+ * v 0.2
  */
 
-CREATE TABLE position(
+CREATE TABLE positions (
 id_position INT(11) NOT NULL AUTO_INCREMENT,
 name TEXT NOT NULL,
+date DATETIME NOT NULL,
 PRIMARY KEY (id_position)
-);
-
+)
