e8736dad113baaee66e48847265d3fb340987b9d
Grib <Grib@192.168.0.26>
2018-07-25 21:32:34 +0300

    Постраничная навигация из БД V 0.1.1

--- page_position.php
+++ page_position.php
@@ -8,17 +8,29 @@
 /**
  * 2018(С) Шевченко Г.Ю.
  * Постраничная навигация из БД
- * V 0.1.0
+ * V 0.1.1
  */
 
     //  Устанавливаем соединение с БД
     require_once("config/config.php");
+
     //  Подключаем классы (постраничной навигации)
     require_once("config/class_config.php");
     
     try{
         //  Объявляем объект постраничнной навигации
-        
-    } catch (Exception $ex) {
-
+        $obj = new pager_mysqli(
+                "postions",
+                "",
+                "ORDER BY name");
+        //  Выводим содержимое текущей страницы
+        $arr = $obj->get_page();
+        for($i = 0; $i < count($arr);$i++)
+        {
+            echo "<a href=position.php?id={$arr[$i][id_position]}>{$arr[$i][name]}</a><br>";
+        }
+        //  Выводим ссылки на другие страницы
+        echo $obj;
+    } catch (ExceptionMySql $ex) {
+        require ("class/ExceptionMySql.php");
     }
