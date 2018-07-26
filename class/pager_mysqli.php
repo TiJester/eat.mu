cd0eada647881b0e53a5b0c9040aee1d5706dc27
admin <admin@reserves.ef.local>
2018-07-26 15:39:43 +0300

    * Описание класа pager_mysqli * Постраничная навигация для базы данных * V 0.1.2

--- class/pager_mysqli.php
+++ class/pager_mysqli.php
@@ -9,15 +9,15 @@
 * 2018(С) Шевченко Г.Ю.
 * Описание класа pager_mysqli
 * Постраничная навигация для базы данных
-* V 0.1.1
+* V 0.1.2
 */
   // Выставляем уровень обработки ошибок 
   error_reporting(E_ALL & ~E_NOTICE);
   
   //    Подключаем все необходимые классы
-  require_once ("config/class_config.php");
+  require_once ("pager.php");
   //    Подклчаем конфиг для подключения к БД
-  require_once ($_SERVER['DOCUMENT_ROOT']."/config/config.php");
+  include ("/../config/config.php");
   
   
 class pager_mysqli extends pager{
@@ -49,13 +49,31 @@ class pager_mysqli extends pager{
                 $this->parameters = $parameters;
    }
    public function get_total() {
+    //  адрес сервера
+    $host = '127.0.0.1'; 
+    
+    //  имя базы данных
+    $database = "eat";
+    
+    //  имя пользователя базы данных
+    $user = "root";
+    
+    //  пароль пользователя к базе данных
+    $password = "";
+    
+    $link = mysqli_connect($host, $user, $password, $database);
+    if($link->connect_errno){
+        echo "Нет подключения к БД: " . $link->connect_errno. " | " . $link->connect_error;
+    }
+       
        //   Формируем запрос на получение общего колличества записей в таблице
-       $query = "SELECT COUNT(*) FROM {$this->tablename} {$this->where} {$this->order}";
-       $result = mysqli_query($link, $query);
-       if(!$result){
-           throw new ExceptionMySql(mysqli_error($link));
-       }
-       return mysqli_result($result, 0);
+       $result_count = mysqli_query($link, "SELECT COUNT(*) FROM {$this->tablename} {$this->where} {$this->order}"); 
+       if($result_count == 0 ){
+            echo "Не работает!";                 }
+         $row = mysqli_fetch_array($result_count);   
+         $count_res = $row[0];
+      // var_dump($count_res);
+       return $count_res;
    }
    
    public function get_pnumber() {
@@ -79,10 +97,12 @@ class pager_mysqli extends pager{
         //  Колличество записей в файле
         $total = $this->get_total();
         //  Вычесляем число страниц в системе
+        //var_dump($number);
         $number = (int)($total/$this->get_pnumber());
         if ((float) ($total / $this->get_pnumber()) - $number != 0) {
             $number++;
         }
+        var_dump($number);      
         //  Проверяем опадает ли запрашиваемый номер страницы в интервал от 1 до get_total()
         if ($page <= 0 || $page > $number) {
             return 0;
@@ -92,11 +112,8 @@ class pager_mysqli extends pager{
         //  Номер, начиная с которого следует выбрать строки файла
         $first = ($page - 1)*$this->get_pnumber();
         //  Извлекаем позиции для текущей страницы
-        $query = "SELECT * FROM ($this->tablename) ($this->where) ($this->order) LIMIT $first, ($this->get_pnumber())";
-        $result = mysqli_query($link, $query);
-        if(!$result){
-            throw new ExceptionMySql(mysqli_error($link), $query, "Ошибка извлечение позиций");
-        }
+        $result = mysqli_query($link, "SELECT * FROM ($this->tablename) ($this->where) ($this->order) LIMIT $first, ($this->get_pnumber())");
+
         //  Если имееться хотя бы один элемент заполняем массив $arr
         if(mysqli_num_rows($result)){
             while ($arr[] = mysqli_fetch_array($result));
