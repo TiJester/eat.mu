91a23248d8c2987d2ad42fbb92071dd901faa9a0
admin <admin@reserves.ef.local>
2018-07-26 15:40:08 +0300

    * position add  * v 0.2

--- SQL/position add.sql
+++ SQL/position add.sql
@@ -6,10 +6,11 @@
  * Author:  admin
  * Created: 25.07.2018
  * position add
- * v 0.1
+ * v 0.2
  */
 
 INSERT INTO positions VALUES (NULL, 'Горячие блюда', '2018-07-25');
 INSERT INTO positions VALUES (NULL, 'Холодные блюда', '2018-07-25');
 INSERT INTO positions VALUES (NULL, 'Дисерт', '2018-07-25');
 INSERT INTO positions VALUES (NULL, 'Выпечка', '2018-07-25');
+INSERT INTO positions VALUES (NULL, 'Закуска', '2018-07-26');
